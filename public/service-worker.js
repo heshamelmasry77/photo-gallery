37bd0cc21feabec0c0bf827581a56e127798ebd1
omer.sheikh <omer.sheikh@afiniti.com>
2018-10-27 01:40:04 +0500

    service worker fixed

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,45 +1,44 @@
 if (workbox) {
   console.log(`Workbox is loaded`);
 
+  workbox.setConfig({
+    debug: false
+  });
+
   workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
+  workbox.routing.registerRoute(
+    /\.(?:png|gif|jpg|jpeg|svg)$/,
+    workbox.strategies.staleWhileRevalidate({
+      cacheName: "images",
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 60,
+          maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
+        })
+      ]
+    })
+  );
+
+  workbox.routing.registerRoute(
+    new RegExp("https://api.flickr.com/services/rest"),
+    workbox.strategies.networkFirst({
+      cacheName: "api"
+    })
+  );
+
+  workbox.routing.registerRoute(
+    new RegExp("https://fonts.(?:googleapis|gstatic).com/(.*)"),
+    workbox.strategies.cacheFirst({
+      cacheName: "googleapis",
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 30
+        })
+      ]
+    })
+  );
+
 } else {
   console.log(`Workbox didn't load`);
 }
-
-workbox.setConfig({
-  debug: false
-});
-
-workbox.precaching.precacheAndRoute([]);
-
-workbox.routing.registerRoute(
-  /\.(?:png|gif|jpg|jpeg|svg)$/,
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: "images",
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 60,
-        maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
-      })
-    ]
-  })
-);
-
-workbox.routing.registerRoute(
-  new RegExp("https://api.flickr.com/services/rest"),
-  workbox.strategies.networkFirst({
-    cacheName: "api"
-  })
-);
-
-workbox.routing.registerRoute(
-  new RegExp("https://fonts.(?:googleapis|gstatic).com/(.*)"),
-  workbox.strategies.cacheFirst({
-    cacheName: "googleapis",
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 30
-      })
-    ]
-  })
-);
